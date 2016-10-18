7f3be1d4f51c89192450988fa06ef77d3833a0c1
jin09 <supergautam995@gmail.com>
2016-10-18 10:43:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 
 
 var img = document.getElementById('madi');
-
+var counter = 0;
 var marginLeft = 0;
 
 function moveRight(){
@@ -12,4 +12,16 @@ function moveRight(){
 
 img.onclick = function(){
     var interval = setInterval(moveRight, 100);
+};
+
+var button = document.getElementById("counter");
+button.onClick = function(){
+    // make request to the counter wndPoint
+    
+    //capture the response of the variable
+    
+    //Render the variable to the correct span
+    counter++;
+    getElementById("count").innerHTML = counter.toString();
+    
 };
