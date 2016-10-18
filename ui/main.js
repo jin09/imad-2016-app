b6d52cec85c5ee18b245bfcc754a0312a9c5b111
jin09 <supergautam995@gmail.com>
2016-10-18 13:26:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,69 +9,74 @@ function moveRight(){
     marginLeft += 10;
     img.style.marginLeft = marginLeft + "px";
 }
-
-img.onclick = function(){
-    var interval = setInterval(moveRight, 100);
-};
+if(img !== null){
+    img.onclick = function(){
+        var interval = setInterval(moveRight, 100);
+    };
+}
 
 var button = document.getElementById("counter");
+if(button !== null){
 button.onclick = function(){
-    // make request to the counter wndPoint
-    var request = new XMLHttpRequest();
-    
-    //capture the response of the variable
-    
-    request.onreadystatechange = function(){
-        if (request.readyState === XMLHttpRequest.DONE) {
-      if (request.status === 200) {
-        document.getElementById("count").innerHTML = request.responseText;
-      }
-      else {
-        alert('There was a problem with the request.');
-      }
-    }
+        // make request to the counter wndPoint
+        var request = new XMLHttpRequest();
+        
+        //capture the response of the variable
+        
+        request.onreadystatechange = function(){
+            if (request.readyState === XMLHttpRequest.DONE) {
+          if (request.status === 200) {
+            document.getElementById("count").innerHTML = request.responseText;
+          }
+          else {
+            alert('There was a problem with the request.');
+          }
+        }
+        };
+        
+        //Make a request
+        request.open('GET', "http://jin09.imad.hasura-app.io/counter");
+        request.send();
     };
-    
-    //Make a request
-    request.open('GET', "http://jin09.imad.hasura-app.io/counter");
-    request.send();
-};
+}
 
 var names;
 
 var submit = document.getElementById("submit");
-submit.onclick = function(){
-    
-        // make request to the counter wndPoint
-    var request = new XMLHttpRequest();
-    
-    //capture the response of the variable
-    
-    request.onreadystatechange = function(){
-        if (request.readyState === XMLHttpRequest.DONE) {
-      if (request.status === 200) {
-        names = request.responseText;
-        names = JSON.parse(names);
-        var list = "";
-        for(var i=0;i<names.length;i++){
-            var name = names[i];
-            list += "<li>"+name+"</li>";
+if(submit !== null){
+    submit.onclick = function(){
+        
+            // make request to the counter wndPoint
+        var request = new XMLHttpRequest();
+        
+        //capture the response of the variable
+        
+        request.onreadystatechange = function(){
+            if (request.readyState === XMLHttpRequest.DONE) {
+          if (request.status === 200) {
+            names = request.responseText;
+            names = JSON.parse(names);
+            var list = "";
+            for(var i=0;i<names.length;i++){
+                var name = names[i];
+                list += "<li>"+name+"</li>";
+            }
+            
+            document.getElementById("list").innerHTML = list;
+            
+          }
+          else {
+            alert('There was a problem with the request.');
+          }
         }
+        };
         
-        document.getElementById("list").innerHTML = list;
+        //Make a request
+        var url = "http://jin09.imad.hasura-app.io/submit-name/";
+        var txt = document.getElementById("name").value;
+        url = url + txt;
+        request.open('GET', url);
+        request.send();
         
-      }
-      else {
-        alert('There was a problem with the request.');
-      }
-    }
     };
-    
-    //Make a request
-    var url = "http://jin09.imad.hasura-app.io/submit-name/";
-    var txt = document.getElementById("name").value;
-    url = url + txt;
-    request.open('GET', url);
-    request.send();
-    
-};
+}
