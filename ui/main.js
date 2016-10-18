72803515e5ced8a71b2a38af6af57e32a121ea64
jin09 <supergautam995@gmail.com>
2016-10-18 09:04:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 alert("HI !!!");
 var txt = document.getElementById("txt");
 txt.innerHTML = "TEXT HACKED!!!";
+
+var img = document.getElementById('madi');
+
+img.onclick = function(){
+    img.style.marginLeft = "100px";
+};
