e21ce65e0e22e067fd681c378ced32fbdb58a76b
jin09 <supergautam995@gmail.com>
2016-10-18 13:22:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,6 +88,8 @@ function createTemplate(data){
         </title>    
         <meta name="viewport" content="width=device-width ,initial-scale=1" />
         <link href="/ui/style.css" rel="stylesheet" />
+        <script type="text/javascript" src="/ui/main.js">
+        </script>
     </head>
     
     <body>
@@ -112,9 +114,6 @@ function createTemplate(data){
         <h2>COMMENTS : </h2>
         <ul id="list">
             </ul>
-        
-        <script type="text/javascript" src="/ui/main.js">
-        </scrpit>
     </body>
     </html> `;
     return htmlTemplate;
