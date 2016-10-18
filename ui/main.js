454d5b7a70cf4cbe3f58ccea6fa589580059208f
jin09 <supergautam995@gmail.com>
2016-10-18 11:31:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,8 +17,19 @@ img.onclick = function(){
 var button = document.getElementById("counter");
 button.onclick = function(){
     // make request to the counter wndPoint
+    var request = new XMLHttpRequest();
     
     //capture the response of the variable
+    
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE) {
+      if (request.status === 200) {
+        alert(request.responseText);
+      } else {
+        alert('There was a problem with the request.');
+      }
+    }
+    };
     
     //Render the variable to the correct span
     counter++;
