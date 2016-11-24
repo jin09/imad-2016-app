f3ee5b86153af5151128cebb68063dbda0cf462b
jin09 <supergautam995@gmail.com>
2016-11-24 11:12:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require("pg").Pool;
+var crypto = require("crypto");
 var config = {
   host: 'db.imad.hasura-app.io',
   user: 'jin09',
@@ -145,6 +146,15 @@ app.get('/counter', function (req, res) {
   res.send(counter.toString());
 });
 
+function hash(input){
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed;
+}
+
+app.get("/hash/:input", function(req, res){
+    var hashString = hash(req.params.input, "this-is-a-random-salt");
+    res.send(hashString);
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
